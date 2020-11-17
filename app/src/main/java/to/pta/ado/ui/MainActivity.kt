8d27f140d6ed813196e017ef063113e923a53c00
Pablo Tato Ramos <pablo.tato@vipera.com>
2020-11-17 15:14:15 +0100

    Move button to bottom right
    and change the implementation a bit hehe

--- app/src/main/java/to/pta/ado/ui/MainActivity.kt
+++ app/src/main/java/to/pta/ado/ui/MainActivity.kt
@@ -4,30 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import to.pta.ado.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
-        supportActionBar?.title = ""
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        R.id.action_manage -> {
+        val navigationButton = findViewById<FloatingActionButton>(R.id.button_navigate_manage)
+        navigationButton.setOnClickListener {
             val intent = Intent(this, ManageActivity::class.java)
             startActivity(intent)
-            true
-        }
-        else -> {
-            super.onOptionsItemSelected(item)
         }
     }
 }
